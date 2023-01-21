4cd033362f514bb0327d53e3a182748f1097eef9
KenjiRage <68568868+KenjiRage@users.noreply.github.com>
2023-01-22 00:59:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// esta es mi función que suma dos números
+// esta es la función que suma dos números
 const sum = (a,b) => {
     return a + b
 }
 
-/ solo un registro en consola para nosotros.
+/ mostramos por consola
 console.log(sum(7,3))
 
 // exporta la función para usarla en otros archivos 
 // (similar a la palabra clave `export` cuando se usa webpack)
 module.exports = { sum };
 
-// one euro is:
+// 1 euro is:
 let oneEuroIs = {
     "JPY": 127.9, // japan yen
     "USD": 1.2, // us dollar
